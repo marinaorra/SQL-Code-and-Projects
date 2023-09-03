da5bff72262213eb1fe8f3d94167ab9a72053433
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-03 17:33:13 +0200

    Update NOT Keyword.sql

--- NOT Keyword.sql
+++ NOT Keyword.sql
@@ -1,4 +1,9 @@
 --Question 1: How many customers aren't 55?
 
+--Example 1:
 SELECT firstname, lastname, gender from customers
 where NOT age='55';
+
+--Example 2:
+SELECT firstname, lastname, gender from customers
+where NOT age='55' and NOT age=54;
