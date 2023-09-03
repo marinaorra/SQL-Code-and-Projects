75f96bffb8015aa21b52d9655148e5e522ddfb78
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-03 16:10:46 +0200

    Create Comments in Code.sql

---
+++ Comments in Code.sql
@@ -0,0 +1,17 @@
+--To make a single line comment use --
+--To make multi line comments make:
+/*
+bla-bla-bla Marina's creation:) big hug and success in learning:)
+*/
+
+
+--Select statement the name of one employee only
+select first_name, last_name from employees
+  --filter on the first name AND last name to limit the amount of data returned
+  --and focus the filtering on single person
+
+where first_name= 'Mayumi' and last_name='Schueller';--filter here on one employee
+/*
+filter on the first name AND last name to limit the amount of data returned
+and focus the filtering on single person
+*/
