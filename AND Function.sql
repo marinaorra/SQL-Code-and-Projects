2d194f64673453ca332818b4a0c2a469861d2c46
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-03 16:08:28 +0200

    Update AND Function.sql

--- AND Function.sql
+++ AND Function.sql
@@ -1,6 +1,10 @@
-/*Task 1:
-*Select an employee with the below Name:
-
-
+--Select statement the name of one employee only
 select first_name, last_name from employees
-where first_name= 'Mayumi' and last_name='Schueller';
+  --filter on the first name AND last name to limit the amount of data returned
+  --and focus the filtering on single person
+
+where first_name= 'Mayumi' and last_name='Schueller';--filter here on one employee
+/*
+filter on the first name AND last name to limit the amount of data returned
+and focus the filtering on single person
+*/
