b08cf82298cfb0860f1f9d8dec741f2d0e7923d5
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-04 23:39:42 +0200

    Create NULL Function and IS Operator.sql

---
+++ NULL Function and IS Operator.sql
@@ -0,0 +1,9 @@
+select 1=1; 
+--Result : true
+
+select null=null
+--Result Null
+
+select first_name, last_name from employees;
+where irst_name is not NULL;
+--Attention: some mistake in above code
