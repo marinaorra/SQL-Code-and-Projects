843d8e9d761861028667f1f14a00e9887727c25a
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-06 20:31:52 +0200

    Update Date and TimeZones.sql

--- Date and TimeZones.sql
+++ Date and TimeZones.sql
@@ -1,2 +1,8 @@
 --in order to check the set timezone of the database run:
 show timezone;
+
+--To establish a timezone for your database to be used as a standard run:
+alter USER postgres set timezone='UTC';
+
+--Reconnect yourdata base and it should be changed to UTC
+
