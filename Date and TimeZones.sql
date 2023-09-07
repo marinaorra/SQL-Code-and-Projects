0b18eafc57d15d6c98bfb2749768e50b36c208fc
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-07 17:55:01 +0200

    Update Date and TimeZones.sql

--- Date and TimeZones.sql
+++ Date and TimeZones.sql
@@ -6,3 +6,23 @@ alter USER postgres set timezone='UTC';
 
 --Reconnect yourdata base and it should be changed to UTC
 
+--insert into timezones Values(
+--TIMESTAMP WITHOUT TIME ZONE '2000-01-01 10:00:00-05',
+--TIMESTAMP WITH TIME ZONE '2000-01-01 10:00:00-05'
+);
+
+
+--To get the current date:
+select NOW()::date;
+or
+select CURRENT_DATE;
+--the date will be showen in a formated in ISO standart(yy-mm-dd) :1800/01/01
+
+
+--format the way of the representaion of the current date format use: TO_CHAR
+SELECT TO_CHAR(current_date, 'dd/mm/yyyyy');
+
+--to get the current week:
+SELECT TO_CHAR(current_date, 'WW');
+
+
