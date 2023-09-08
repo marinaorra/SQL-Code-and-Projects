f04e4709c8315d2c016a582440b7a4333bd8c5de
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-08 18:35:05 +0200

    Update Intervals.sql

--- Intervals.sql
+++ Intervals.sql
@@ -24,7 +24,28 @@ extract(
 --/*
 * DB: Employees
 * Table: employees
-* Question: Get me all the employees above 60, use the appropriate date functions
+  
+--Question: How many employees where hired in February?
+SELECT count(emp_no) FROM employees
+where EXTRACT (MONTH FROM hire_date) = 2;
+
+
+--Question: How many employees were born in november?
+select count(emp_no)
+from employees
+WHERE extract(month from birth_date)=11;
+
+
+* Question: Who is the oldest employee?
+SELECT MAX(AGE(birth_date)) FROM employees;
+
+
+/*
+* DB: Store
+* Table: orders
+* Question: How many orders were made in January 2004?
 */
 
-SELECT * FROM employees;
+SELECT COUNT(orderid)
+FROM orders
+WHERE DATE_TRUNC('month', orderdate) = date '2004-01-01';
