6db38d12c679ecd1a0b631ac8cc1c728bd42b28e
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-11 17:26:32 +0200

    Update Group By.sql

--- Group By.sql
+++ Group By.sql
@@ -84,3 +84,19 @@ inner join departments as d using(dept_no)
 where e.gender='F'
 group by  d.dept_name
 order by dept_name, count(e.emp_no) desc;
+
+
+--Question 4:
+--Question 4:
+/*
+*  Show me all the femaile employees that work in the department different developments sorted by the highest amount number in the department;
+*  Database: Employees
+*/
+
+SELECT d.dept_name, count(e.emp_no) as "amount of employess"
+FROM employees as e
+inner join dept_emp as de USING(emp_no)
+inner join departments as d using(dept_no)
+where e.gender='F'
+group by  d.dept_name
+order by dept_name, count(e.emp_no) desc;
