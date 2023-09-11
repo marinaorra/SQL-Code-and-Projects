cca9e0840b348b4bf760d2d1ef0029f321482096
marinaorra <117170362+marinaorra@users.noreply.github.com>
2023-09-11 15:26:10 +0200

    Update HAVING.sql

--- HAVING.sql
+++ HAVING.sql
@@ -18,3 +18,17 @@ having col1 ===Y;
 --So WHERE applies filters to the individual roles, 
 --and HAVING applies filters to a group as a whole
 
+Example:
+/*
+*  Show me all amount of female employees that work in the departments where number of employees >25000
+*  Database: Employees
+*/
+
+SELECT d.dept_name, count(e.emp_no) as "amount of employess"
+FROM employees as e
+inner join dept_emp as de USING(emp_no)
+inner join departments as d using(dept_no)
+where e.gender='F'
+group by  d.dept_name
+having count(e.emp_no)>25000;
+
